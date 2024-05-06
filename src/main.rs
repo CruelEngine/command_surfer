af13799ab717ac3f5773d445be4ed7d500a53468
CruelEngine <babhineethbhat@gmail.com>
2024-05-06 15:48:20 +0530

    feat: Added serde crate and added a method to parse package.json file in root directory

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::path::Path;
+
+fn main() {
+    let _result = parse_package();
+}
+
+
+
+fn parse_package() {
+    // Build the package.json file path
+    let file_path = Path::new("package.json");
+}
