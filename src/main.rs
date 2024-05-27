f16f14988d9116521ebbabec779523f123b0004d
CruelEngine <babhineethbhat@gmail.com>
2024-05-27 22:59:04 +0530

    feat: Added logic to handle keyboard events to go over list, quit the script

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 use std::process::{Command, Stdio};
-use ncurses::{COLOR_BLACK, COLOR_WHITE, curs_set, CURSOR_VISIBILITY, endwin, init_pair, initscr, noecho, refresh, start_color, getch};
+use ncurses::{COLOR_BLACK, COLOR_WHITE, curs_set, CURSOR_VISIBILITY, endwin, init_pair, initscr, noecho, refresh, start_color, getch, attron, COLOR_PAIR};
 use ncurses::addstr;
+use ncurses::mv;
 use serde::{Deserialize, Serialize};
 
 
@@ -29,6 +30,7 @@ fn main() {
 
 
 fn parse_package() {
+    let mut quit = false;
 
     initscr();
     noecho();
@@ -62,16 +64,44 @@ fn parse_package() {
 
     let json_value: Package = serde_json::from_str(&json_string).expect("Failed to parse json");
 
-    let selected_command_index = 0;
+    let mut selected_command_index = 0;
 
     // Display List of executable scripts
 
+    let script_list = json_value.scripts.keys();
     for (_index, key) in json_value.scripts.keys().enumerate() {
         let script_name: String = format!("npm run {}", key);
-        addstr(&script_name).unwrap();
+        mv(_index as i32, 0 as i32);
+        attron(COLOR_PAIR(REGULAR_PAIR));
+        addstr(&script_name);
         println!("{}", script_name);
     }
-    getch();
+    while !quit {
+
+    let key = getch();
+
+        match key as u8 as char {
+            'q' => quit = true,
+            'w' => {
+                selected_command_index -= 1;
+                if selected_command_index < 0{
+                    selected_command_index = script_list.len() - 1;
+                }
+            },
+            's' => {
+                selected_command_index += 1;
+                if script_list.len() < selected_command_index {
+                    selected_command_index = 0;
+                }
+            },
+            '\n' => {
+                println!("{}",selected_command_index);
+            },
+            _ => {
+
+            }
+        }
+    }
     endwin();
 
 }
@@ -79,9 +109,6 @@ fn parse_package() {
 fn execute_command(npm_command: &str) {
     let mut command = Command::new("sh");
     command.arg("-c").arg(npm_command);
-
     command.stdin(Stdio::inherit()).stdout(Stdio::inherit()).stderr(Stdio::inherit());
-
-
     command.spawn().expect("failed to spawn sh process").wait().expect("failed to wait for sh process");
 }
