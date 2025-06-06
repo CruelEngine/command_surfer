aa7d1ba4c933e0ce980ff63b7952731601ab1852
CruelEngine <babhineethbhat@gmail.com>
2025-06-06 15:42:41 +0530

    refactor: refactored the methods to remove redundant params

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,12 @@ use pancurses::{
     curs_set, endwin, init_pair, initscr, noecho, start_color, ColorPair, Input, Window,
     COLOR_BLACK, COLOR_WHITE,
 };
-use std::{env, io::Error};
+use std::env;
 
 use node_script_list::{
     execute_command, get_package_manager_prefix, parse_package_json_file, sort_command_list,
     CommandPrefix, Mode,
 };
-
-fn main() {
-    match App::new() {
-        Ok(mut app) => {
-            app.run();
-        }
-        Err(err) => {
-            eprintln!("Error: {}", err);
-            std::process::exit(1);
-        }
-    };
-}
 
 enum ColorScheme {
     Regular = 0,
@@ -52,41 +40,41 @@ impl App {
             env::current_dir().map_err(|e| format!("Failed to get current directory {}", e))?;
         let json_value = parse_package_json_file(&current_directory)
             .ok_or_else(|| "No package.json found or failed to parse".to_string())?;
-        let mut highlighted_command_index: usize = 0;
         let package_manager_prefix = get_package_manager_prefix(&current_directory);
         let prefixed_script_list: Vec<String> = json_value.prefix_command(package_manager_prefix);
         let commands = sort_command_list(prefixed_script_list);
         let window = initscr();
-        let mut mode: Mode = Mode::DEFAULT;
-        let mut filter_string: String = String::new();
         noecho();
         curs_set(0);
         start_color();
         ColorScheme::init();
 
         Ok(App {
-            highlighted_command_index,
+            highlighted_command_index: 0,
             commands,
             window,
-            mode,
-            filter_string,
+            mode: Mode::DEFAULT,
+            filter_string: String::new(),
             quit: false,
         })
     }
 
     fn run(&mut self) {
-        let mut filtered_commands: Vec<String> = self.commands.clone();
+        let filtered_commands: Vec<String> = self.commands.clone();
         while !self.quit {
             self.display_commands(&filtered_commands);
-            self.display_filter_value(filtered_commands.len() as i32);
-            self.handle_keyboard_input(&mut filtered_commands);
+            match self.mode {
+                Mode::FILTER => self.display_filter_value(filtered_commands.len() as i32),
+                _ => {}
+            }
+            self.handle_keyboard_input();
         }
     }
 
-    fn handle_keyboard_input(&mut self, filtered_commands: &mut Vec<String>) {
+    fn handle_keyboard_input(&mut self) {
         let key = self.window.getch();
         match self.mode {
-            Mode::FILTER => self.handle_filter_mode(filtered_commands, key),
+            Mode::FILTER => self.handle_filter_mode(key),
             Mode::DEFAULT => self.handle_default_mode(key),
         }
     }
@@ -123,14 +111,13 @@ impl App {
         }
     }
 
-    fn handle_filter_mode(&mut self, filtered_commands: &mut Vec<String>, key: Option<Input>) {
+    fn handle_filter_mode(&mut self, key: Option<Input>) {
         match key {
             None => {}
             Some(Input::Character('\x1B')) => {
                 self.mode = Mode::DEFAULT;
                 self.filter_string.clear();
-                *filtered_commands = self.commands.clone();
-                self.display_commands(&*filtered_commands);
+                self.filter_commands(&self.filter_string);
             }
             Some(Input::Character('\n')) => {
                 self.quit = true;
@@ -140,27 +127,23 @@ impl App {
             Some(Input::KeyBackspace) => {
                 self.filter_string.pop();
                 let filter_pattern: &str = &self.filter_string;
-                *filtered_commands = self.commands.clone();
-                self.filter_commands(filtered_commands, filter_pattern);
+                self.filter_commands(filter_pattern);
             }
             Some(Input::Character('\x7f')) => {
                 self.filter_string.pop();
                 let filter_pattern: &str = &self.filter_string;
-                *filtered_commands = self.commands.clone();
-                self.filter_commands(filtered_commands, filter_pattern);
+                self.filter_commands(filter_pattern);
             }
             Some(Input::KeyDC) => {
                 self.filter_string.pop();
                 let filter_pattern: &str = &self.filter_string;
-                *filtered_commands = self.commands.clone();
-                self.filter_commands(filtered_commands, filter_pattern);
+                self.filter_commands(filter_pattern);
             }
             Some(Input::Character(character)) => {
                 if character.is_alphanumeric() || character == ' ' {
                     self.filter_string.push(character);
                     let filter_pattern: &str = &self.filter_string;
-                    *filtered_commands = self.commands.clone();
-                    self.filter_commands(filtered_commands, filter_pattern);
+                    self.filter_commands(filter_pattern);
                 }
             }
             _ => {
@@ -169,8 +152,10 @@ impl App {
         }
     }
 
-    fn filter_commands(&self, filtered_commands: &mut Vec<String>, filter_pattern: &str) {
-        *filtered_commands = filtered_commands
+    fn filter_commands(&self, filter_pattern: &str) {
+        let filtered_commands = self
+            .commands
+            .clone()
             .iter()
             .filter(|comand| comand.contains(filter_pattern))
             .cloned()
@@ -200,4 +185,16 @@ impl App {
             .addstr(format!("filtered value: {}", self.filter_string));
         self.window.refresh();
     }
+}
+
+fn main() {
+    match App::new() {
+        Ok(mut app) => {
+            app.run();
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            std::process::exit(1);
+        }
+    };
 }
